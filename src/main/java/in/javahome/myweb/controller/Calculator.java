4b377ab10fd9187c6e85961057482513dd5f9c73
Amine KAROUI <karoui_amine@carrefour.com>
2020-05-14 05:38:12 +0200

    update

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -3,10 +3,13 @@ package in.javahome.myweb.controller;
  * 
  */
 public class Calculator {
-	int a;
-	int b;
-	void a987aeaes() {
+	
+	void test() {
+        int target = -5;
+int num = 3;
 
+target =- num;  // Noncompliant; target = -3. Is that really what's meant?
+target =+ num; // Noncompliant; target = 3
 	}
 	/*
 	 * @param i
